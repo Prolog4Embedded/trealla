b9f46a686d635f5c608e104842fe2ea38ab16d15
Simon Blum <simon21.blum@gmail.com>
2026-04-23 11:06:04 +0200

    fix: remove cpu from tpl_board function

--- boards/generic/board.cmake
+++ boards/generic/board.cmake
@@ -1,7 +1,6 @@
 tpl_board(
   NAME         "generic"
   DESCRIPTION  "Generic ARM Cortex-M4 template board"
-  CPU          "cortex-m4"
   LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/linker_script.ld"
   SOURCES      "${CMAKE_CURRENT_LIST_DIR}/time.c"
   INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}"
--- boards/mps3-an524/board.cmake
+++ boards/mps3-an524/board.cmake
@@ -1,7 +1,6 @@
 tpl_board(
   NAME                "mps3-an524"
   DESCRIPTION         "ARM Cortex-M33, QEMU mps3-an524, picolibc + semihosting"
-  CPU                 "cortex-m33"
   LINKER_SCRIPT       "${CMAKE_CURRENT_LIST_DIR}/linker_script.ld"
   PICOLIBC_CROSS_FILE "${CMAKE_CURRENT_LIST_DIR}/picolibc-cross.txt"
   USE_SEMIHOST        ON
